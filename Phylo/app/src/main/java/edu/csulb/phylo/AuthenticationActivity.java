4e62f9a4be3ad256f5df03aab0555840034f8640
Daniel <danielkimstudent@hotmail.com>
2018-01-24 11:08:55 -0800

    Initialize Job for Viet

---
+++ Phylo/app/src/main/java/edu/csulb/phylo/AuthenticationActivity.java
@@ -0,0 +1,10 @@
+package edu.csulb.phylo;
+
+import android.app.Activity;
+
+/**
+ * Created by Danie on 1/24/2018.
+ */
+
+public class AuthenticationActivity extends Activity {
+}
---
+++ Phylo/app/src/main/java/edu/csulb/phylo/LoginFragment.java
@@ -0,0 +1,31 @@
+package edu.csulb.phylo;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+
+/**
+ * Created by Danie on 1/24/2018.
+ */
+
+public class LoginFragment extends Fragment
+    implements View.OnClickListener{
+    //TODO: create variables here
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        //TODO: Initialize every button/ text box/ variables here
+
+        //TODO: Initialize Listeners Here for buttons
+    }
+
+    @Override
+    public void onClick(View v) {
+        //TODO: Switch case the ID's
+        switch(v.getId()) {
+
+        }
+    }
+}
