83aa2eaacc1417b66b5a77096a88a9d321d3c2e8
Daniel Kim <danielkimstudent@hotmail.com>
2018-01-28 16:01:31 -0800

    Add User Permission and Map Functionality

--- Phylo/app/src/main/java/edu/csulb/phylo/LoginFragment.java
+++ Phylo/app/src/main/java/edu/csulb/phylo/LoginFragment.java
@@ -1,9 +1,13 @@
 package edu.csulb.phylo;
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * Created by Danie on 1/24/2018.
@@ -11,21 +15,36 @@ import android.view.View;
 
 public class LoginFragment extends Fragment
     implements View.OnClickListener{
-    //TODO: create variables here
+    //Views
+    private Button loginButton;
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_login, container, false);
+    }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        //TODO: Initialize every button/ text box/ variables here
 
-        //TODO: Initialize Listeners Here for buttons
+        //Initialize Views
+        loginButton = (Button) getActivity().findViewById(R.id.button_normal_login);
+
+
+        //Initialize Listeners
+        loginButton.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-        //TODO: Switch case the ID's
         switch(v.getId()) {
-
+            case R.id.button_normal_login: {
+                Intent intent = new Intent(getActivity(), MapsActivity.class);
+                startActivity(intent);
+            }
+            break;
         }
     }
 }
