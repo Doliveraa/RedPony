35275762b48321fa0933451e2dd1c1824532a927
Daniel Olivera <dannyolivera11@gmail.com>
2018-02-12 09:34:43 -0800

    fixed bug

--- app/router.js
+++ app/router.js
@@ -2,68 +2,67 @@ const USER = require('./schemas/user');
 const FILE = require('./schemas/file');
 let router;
 
-const routing = function (express_router) {
+const routing = function routing(express_router) {
     router = express_router;
 
-    router.route('/').get(function (request, response) {
+    router.route('/').get(function (req, res) {
         res.json({ message: 'welcome to the rest api' });
     });
 
-
     //get a user using userid and homeid
-    router.route('/:homeid/users/:userid').get(function (request, response) {
-        USER.find({ homeid: request.params.homeid, userid: request.params.userid }, function (err, user) {
-            response.json(user);
+    router.route('/:homeid/users/:userid').get(function (req, res) {
+        USER.find({ homeid: req.params.homeid, userid: req.params.userid }, function (err, user) {
+            res.json(user);
         });
     });
 
     //create a user
-    router.route('/:homeid/users').post(function (request, response) {
+    router.route('/:homeid/users').post(function (req, res) {
         var user = new USER();
-        user.userid = request.body.userid;
-        user.homeid = request.params.homeid;
+        user.userid = req.body.userid;
+        user.homeid = req.params.homeid;
 
         user.save(function (error) {
             if (err)
-                response.send(error);
+                res.send(error);
 
             res.json({ message: 'User created' });
         });
     });
 
     //create a file
-    router.route('/:homeid/users/:userid/files').post(function (request, response) {
+    router.route('/:homeid/users/:userid/files').post(function (req, res) {
         var file = new FILE();
-        file.userid = request.params.userid;
-        file.homeid = request.params.homeid;
-        file.name = request.body.name;
-        file.location = request.body.location;
-        file.experiation = request.body.experiation;
-        file.data = request.body.data;
+        file.userid = req.params.userid;
+        file.homeid = req.params.homeid;
+        file.name = req.body.name;
+        file.location = req.body.location;
+        file.experiation = req.body.experiation;
+        file.data = req.body.data;
 
         file.save(function (error) {
             if (err)
-                response.send(error);
+                res.send(error);
 
             res.json({ message: 'file created' });
         });
     });
 
     //get a users created files using homeid and userid
-    router.route('/:homeid/users/:userid/files').get(function (request, response) {
-        FILE.find({ homeid: request.params.homeid, userid: request.params.userid }, function (err, files) {
-            response.json(files);
+    router.route('/:homeid/users/:userid/files').get(function (req, res) {
+        FILE.find({ homeid: req.params.homeid, userid: req.params.userid }, function (err, files) {
+            res.json(files);
         });
     });
 
     //get files near a users location
-    router.route('/:homeid/users/:userid/files').get(function (request, response) {
+    router.route('/:homeid/users/:userid/files').get(function (req, res) {
 
         //****** this VVV is placeholder code we need to decide how to handle radius look into Haversine formula
-        var min = request.body.locationX + 10;
-        var max = request.body.locationY - 10;
+        var min = req.body.locationX + 10;
+        var max = req.body.locationY - 10;
         FILE.find({ location: { $gt: min, $lt: max } }, function (err, files) {
-            response.json(files);
+            res.json(files);
         });
     });
 
