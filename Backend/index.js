4622df343335051a1e16080e1936da4784308dbf
Daniel Olivera <dannyolivera11@gmail.com>
2018-02-26 14:52:10 -0800

    added body parser

--- Backend/index.js
+++ Backend/index.js
@@ -2,14 +2,22 @@ const MONGOOSE = require('mongoose');
 const EXPRESS = require('express');
 const DB_CONFIG = require('./config/mongo');
 const ROUTES = require('./app/router');
+const BODY_PARSER = require('body-parser');
 
 const app = EXPRESS();
 const port = 8080;
+
+app.use(BODY_PARSER.urlencoded({
+  parameterLimit: 100000000,
+  limit: '10000kb',
+  extended: true,
+}));
 
 const ROUTER = ROUTES(EXPRESS.Router());
 app.use('/',ROUTER);
 
 app.listen(port);
+console.log('Running on port: '+port);
 
 console.log('Hello from rest api running on port: '+port);
 
