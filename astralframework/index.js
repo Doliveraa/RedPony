b3cceee5ce8ce24c0118bf5173f5267a36e57ca1
Jared Coleman <jaredcoleman916@gmail.com>
2018-02-25 17:18:04 -0800

    Adds addFile function

--- astralframework/index.js
+++ astralframework/index.js
@@ -1,19 +1,75 @@
+var moment = require('moment');
+moment().format();
 
+// Helper methods
+function isString (value) {
+	return typeof value === 'string' || value instanceof String;
+};
+
+// Returns if a value is a boolean
+function isBoolean (value) {
+	return typeof value === 'boolean';
+};
+
+function isLocation (lat, lon) {
+	return (lat >= -90 && lat <= 90 && lon >= -180 && lon <= 180);
+}
+
+/**
+ * Astral class
+ * @param key
+ */
 class Astral {
     constructor(key) {
 		this.key = key;
         console.log('Astral constructor');
     }
-
-    addFileType() {
+    
+    /**
+     * Adds a file type 
+     * @param {FileType} ft file type
+     */
+    addFileType(f) {
         console.log('Adding file type');
     }
+    
+    confirmType(type, data) {
+		//get type from database, if it doesn't exist return false
+		//check that data adheres to type
+		return true
+	}
 
-    addFile() {
-        console.log('Adding file');
+	/**
+	 * Adds a file
+	 * @param {File} f file
+	 */
+    addFile(name, type, lat, lon, expiration, data) {
+		expiration = moment(expiration);
+		if (isString(name) && isString(type) && isLocation(lon, lat) 
+			&& expiration.isValid() && this.confirmType(type, data)) {
+				var file = {
+				name: name,
+				type: type,
+				location: {lon: lon, lat: lat},
+				expiration: expiration,
+				data: data
+			};
+			
+			//TODO: send file 
+			console.log(file);
+		} else {
+			console.log('Invalid file. Check type.');
+		}
+		
     }
 
-    getFiles(location, owner=null, maximum=null) {
+	/**
+	 * Get files, given restrictions
+	 * @param {string} location location restriction
+	 * @param {string} [null] owner userid of file owner
+	 * @param {number} [50] maximum number of files to get
+	 */
+    getFiles(location, owner=null, maximum=50) {
         console.log('getting files at %s', location);
     }
 }
--- astraltester/index.js
+++ astraltester/index.js
@@ -1,6 +1,5 @@
-var ast = require('../astralframework/astralframework');
+var ast = require('../astralframework');
 
 conn = ast.connect();
-conn.addFileType();
-conn.addFile();
-conn.getFiles('32.101, 101.568');
+conn.addFile('hello_image',  'image', '-8.945406', '38.575078', '2018-03-25T12:00:00Z', {path: './hello.jpg', caption: 'hello toast'});
+conn.getFiles('-8.945406, 38.575078');
