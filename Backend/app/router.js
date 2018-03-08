4578144ccf6236e8314f45ed91f1756514f79534
Daniel Olivera <dannyolivera11@gmail.com>
2018-03-08 00:56:58 -0800

    updated routes, added user and home verification, user ids, use of json objects and query in parameters

--- Backend/app/router.js
+++ Backend/app/router.js
@@ -1,5 +1,7 @@
 const USER = require('./schemas/user');
+const HOMEBASE = require('./schemas/homebase');
 const FILE = require('./schemas/file');
+const UUID = require('uuid/v1');
 const MIDDLEWARE = require('./middleware');
 
 let router;
@@ -10,53 +12,133 @@ const routing = function routing(express_router) {
         res.json({ message: 'welcome to the rest api' });
     });
 
-
-
-    //get a user using userid and homeid
-    router.route('/:homeid/users/:userid').get(function (req, res) {
-        USER.find({ homeid: req.params.homeid, userid: req.params.userid }, function (err, user) {
+    //get a user using homeid in the http params and userid in the request body
+    router.route('/users/:homeid').get(function (req, res) {
+        USER.find({ homeid: req.params.homeid, userid: req.body.userid }, function (err, user) {
             res.json(user);
         });
     });
 
-    //create a user
-    router.route('/users/:homeid').post(function (req, res) {
+    //create a user (using promises)
+    router.route('/promises/users/:homeid').post(function (req, res) {
       console.log('Router: create user ');
-      console.log(req.body);
-      //console.log(req.params.homeid);
-      //console.log(req.body.userid);
+      console.log(req.params.homeid);
+      console.log(req.body.userid);
       MIDDLEWARE.createUser(req, res)
         .then(result => res.json(result))
-        .catch(error => {console.log(error); res.json(error)});
+        .catch(err => {console.log(err); res.json(err)});
+    });
+
+    //create a user (not using promises)
+    router.route('/users').post(function (req, res) {
+      console.log('Router: create user ');
+
+      HOMEBASE.findOne({ homeid: req.body.homeid },function (err, homebase) {
+        if(err) {
+          res.send(err);
+        }
+        //if homebase not found send error
+        if(!homebase) {
+          console.log('error: homeid invalid');
+          res.json({ error: 'homeid invalid' });
+        }
+        //if homebase exists create user and return success message
+        else {
+          console.log('creating new user');
+          var newUser = new USER();
+          newUser.userid = req.body.userid;
+          newUser.homeid = req.body.homeid;
+          newUser.save(function (err) {
+            if (err)
+              res.send(err);
+            res.json({ message: 'user created' });
+          });
+        }
+      });
+    });
+
+    // create homebase
+    router.route('/homebase').post(function (req, res) {
+      console.log('Router: create homebase');
+      HOMEBASE.findOne({ appName: req.body.appName },function (err, homebase) {
+        if(err) {
+          res.send(err);
+        }
+        //if homebase not found create one
+        if(!homebase) {
+          console.log('creating new homebase');
+          var newHome = new HOMEBASE();
+          newHome.appName = req.body.appName;
+          newHome.homeid = UUID();
+          newHome.save(function(err, homebase) {
+            if (err){
+              res.send(err);
+            }
+            res.json({ homeid: homebase.homeid });
+          });
+        }
+        //if homebase exist return message
+        else {
+          console.log('homebase found');
+          res.json({ message: "homebase exists" });
+        }
+      });
+
     });
 
     //create a file
-    router.route('/:homeid/users/:userid/files').post(function (req, res) {
-        var file = new FILE();
-        file.userid = req.params.userid;
-        file.homeid = req.params.homeid;
-        file.name = req.body.name;
-        file.location = req.body.location;
-        file.experiation = req.body.experiation;
-        file.data = req.body.data;
+    router.route('/files').post(function (req, res) {
+      console.log('create file');
+      USER.findOne({ userid: req.body.userid, homeid: req.body.homeid },function (err, user) {
+        if(err) {
+          res.send(err);
+        }
+        //if user not found send error
+        if(!user) {
+          console.log('error: userid invalid');
+          res.json({ error: 'userid invalid' });
+        }
 
-        file.save(function (error) {
+        if(user){
+          var file = new FILE();
+          file.homeid = req.body.homeid;
+          file.userid = req.body.userid;
+          file.name = req.body.name;
+          file.location = req.body.location;
+          file.expirationDate = new Date(req.body.expirationDate);
+
+          file.save(function (err) {
             if (err)
-                res.send(error);
+              res.send(err);
 
             res.json({ message: 'file created' });
-        });
+          })
+        }
+      });
+
     });
 
     //get a users created files using homeid and userid
-    router.route('/:homeid/users/:userid/files').get(function (req, res) {
-        FILE.find({ homeid: req.params.homeid, userid: req.params.userid }, function (err, files) {
-            res.json(files);
-        });
+    router.route('/files').get(function (req, res) {
+      console.log('get user owned files');
+      FILE.find({ userid: req.query.userid, homeid: req.query.homeid  }, function (err, files) {
+        if (err){
+          res.send(err);
+        }
+
+        //if files not found send message
+        if(!files.length) {
+          console.log('user has no files');
+          res.json({ message: 'user has no files' });
+        }
+        else{
+          res.json(files);
+        }
+      });
     });
 
     //get files near a users location
-    router.route('/:homeid/users/:userid/files').get(function (req, res) {
+    router.route('/files/nearby/:userid').get(function (req, res) {
 
         //****** this VVV is placeholder code we need to decide how to handle radius look into Haversine formula
         var min = req.body.locationX + 10;
