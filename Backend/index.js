7dddf4460c556aa5e2e8a744ba1b7760a0d716ac
Daniel Olivera <dannyolivera11@gmail.com>
2018-03-08 00:57:37 -0800

    changed url encoded for body parser, added json to requests

--- Backend/index.js
+++ Backend/index.js
@@ -6,12 +6,7 @@ const BODY_PARSER = require('body-parser');
 
 const app = EXPRESS();
 const port = 8080;
-
-app.use(BODY_PARSER.urlencoded({
-  parameterLimit: 100000000,
-  limit: '10000kb',
-  extended: true,
-}));
+app.use(BODY_PARSER.urlencoded({ extended: true }));
 app.use(BODY_PARSER.json());
 
 const ROUTER = ROUTES(EXPRESS.Router());
@@ -19,7 +14,5 @@ app.use('/',ROUTER);
 
 app.listen(port);
 console.log('Running on port: '+port);
-
-console.log('Hello from rest api running on port: '+port);
 
 MONGOOSE.connect(DB_CONFIG.path);
