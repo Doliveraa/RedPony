85b6b66b18ce94bcb49c6340858247d320726abd
Jared Coleman <jaredcoleman916@gmail.com>
2018-03-12 14:15:46 -0700

    Moves documentation generator into tools directory

--- gendocs.sh
+++
@@ -1,2 +0,0 @@
-documentation build astralframework/index.js -f html -o ./html
-documentation readme astralframework/index.js --readme-file=./astralframework/README.md --section=API
---
+++ tools/gendocs.sh
@@ -0,0 +1,2 @@
+documentation build ../astralframework/index.js -f html -o ./html
+documentation readme ../astralframework/index.js --readme-file=./astralframework/README.md --section=API
