aea52308ae90f7ddee50bcb36061214b07443660
Ubuntu <ubuntu@ip-172-31-41-119.us-east-2.compute.internal>
2018-03-31 22:41:04 +0000

    Adds help text for astral commands

--- astral/commands.py
+++ astral/commands.py
@@ -6,35 +6,45 @@ def main():
     pass
 
 @click.command(name='get-app-token')
-@click.option('--file', default=None)
-@click.argument('name')
-def get_app_token(file, name):
-    astral.get_app_token(file, name)
+@click.option('--file', default=None, help="File to write token to.",
+              metavar="<filepath>")
+@click.argument('app')
+def get_app_token(file, app):
+    """Gets the json web token for APP."""
+    astral.get_app_token(file, app)
 
 @click.command(name='add-app')
-@click.option('--file', default=None)
-@click.argument('name')
-def add_app(file, name):
-    astral.add_app(file, name)
+@click.option('--file', default=None, help="File to write token to.", 
+              metavar="<filepath>")
+@click.argument('app')
+def add_app(file, app):
+    """Adds APP and returns its json web token."""
+    astral.add_app(file, app)
 
 @click.command(name='setup-api')
-@click.option('--port')
-@click.option('--secret')
-@click.option('--file', default='./config.json')
+@click.option('--port', help="Port to setup API at.", metavar="<integer>")
+@click.option('--secret', help="Secret to use for json web tokens", 
+              metavar="<string>")
+@click.option('--file', default='./config.json', help="File to write " + \
+              "configs to.", metavar="<filepath>")
 def setup_api(port, secret, file):
+    """Creates api config at FILE with PORT and SECRET."""
     astral.setup_api(port, secret, file)
 
 @click.command(name='start')
 def start():
+    """Starts the api."""
     astral.run_script('start.sh')
 
 @click.command(name='restart')
 def restart():
+    """Restarts the api."""
     astral.run_script('stop.sh')
     astral.run_script('start.sh')
 
 @click.command(name='stop')
 def stop():
+    """Stops the api."""
     astral.run_script('stop.sh')
 
 main.add_command(get_app_token)
