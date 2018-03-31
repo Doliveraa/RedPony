03de91eb746ade6af37b0b605a68e3769003fd70
Ubuntu <ubuntu@ip-172-31-41-119.us-east-2.compute.internal>
2018-03-31 22:26:03 +0000

    Fixes issues with setup script

--- setup.sh
+++ setup.sh
@@ -1,30 +1,38 @@
-# Install dependencies
-sudo apt-get install python3 python3-pip curl
+#!/bin/bash
+SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
+echo $SCRIPT_DIR
+sudo apt-get update
+sudo apt-get install curl
 
-# Install astral
-cd astral
-pip3 install .
-cd ..
+# Install node 
+cd ~
+curl -sL https://deb.nodesource.com/setup_8.x -o nodesource_setup.sh
+sudo bash nodesource_setup.sh
+sudo apt-get install nodejs
+rm nodesource_setup.sh
 
-# Install Node.js with nvm
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
-source ~/.bashrc
-. ~/.nvm/nvm.sh
-nvm install --lts
+#Install python
+sudo apt-get install python3 python3-pip
 
-# Install and start MongoDB
+# Install mongodb
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
 echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
 sudo apt-get update
-sudo apt-get install -y mongodb-org
+sudo apt-get install -y mongodb-org 
 sudo systemctl start mongod
 sudo systemctl enable mongod
 
+# Install astral
+cd $SCRIPT_DIR/astral
+pip3 install -e .
+
 # Generate config file
 read -p 'Enter a secret phrase: ' secret_phrase
-astral setup-api --port 80 --secret $secret_phrase
+astral setup-api --port 80 --secret "$secret_phrase"
+cp $SCRIPT_DIR/astral/config.json $SCRIPT_DIR/API/config/config.json
 
 # Install node packages
-cd API
+cd $SCRIPT_DIR/API
 npm install
-npm install forever
+sudo npm install -g forever
+
