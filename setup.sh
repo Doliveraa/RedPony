68f0e8e95c4cc3a3d7d8451760b45ef8ed02a1ad
Jared Coleman <jaredraycoleman@gmail.com>
2018-03-31 16:18:15 -0700

    Updates setup script

--- setup.sh
+++ setup.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
-echo $SCRIPT_DIR
+
+# Install curl
 sudo apt-get update
 sudo apt-get install curl
 
-# Install node 
+# Install Node.js 
 cd ~
 curl -sL https://deb.nodesource.com/setup_8.x -o nodesource_setup.sh
 sudo bash nodesource_setup.sh
 sudo apt-get install nodejs
 rm nodesource_setup.sh
 
-#Install python
+# Install Python
 sudo apt-get install python3 python3-pip
 
 # Install mongodb
@@ -28,7 +29,8 @@ pip3 install -e .
 
 # Generate config file
 read -p 'Enter a secret phrase: ' secret_phrase
-astral setup-api --port 80 --secret "$secret_phrase"
+read -p 'Enter a port for API: ' port
+astral setup-api --port $port --secret "$secret_phrase"
 cp $SCRIPT_DIR/astral/config.json $SCRIPT_DIR/API/config/config.json
 
 # Install node packages
