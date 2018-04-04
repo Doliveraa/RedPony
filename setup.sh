9fc33bef141461e88cd020ef38e2a53c05fe88d9
Jared Coleman <jaredraycoleman@gmail.com>
2018-04-03 19:08:56 -0700

    Enables autocompletion for astral tool

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
 sudo apt-get update
 sudo apt-get install curl
 
-# Install Node.js 
+# Install Node.js
 cd ~
 curl -sL https://deb.nodesource.com/setup_8.x -o nodesource_setup.sh
 sudo bash nodesource_setup.sh
@@ -19,13 +19,15 @@ sudo apt-get install python3 python3-pip
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
 echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
 sudo apt-get update
-sudo apt-get install -y mongodb-org 
+sudo apt-get install -y mongodb-org
 sudo systemctl start mongod
 sudo systemctl enable mongod
 
 # Install astral
 cd $SCRIPT_DIR/astral
-pip3 install -e .
+pip3 install -e --upgrade --force-reinstall .
+if grep -Fxq ".*_ASTRAL_COMPLETE.*"
+echo "eval $(_ASTRAL_COMPLETE=source astral)" >> ./bashrc
 
 # Generate config file
 read -p 'Enter a secret phrase: ' secret_phrase
@@ -37,4 +39,3 @@ cp $SCRIPT_DIR/astral/config.json $SCRIPT_DIR/API/config/config.json
 cd $SCRIPT_DIR/API
 npm install
 sudo npm install -g forever
-
