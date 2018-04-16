76b34aca94951c5e7509f7d67892c528aa314bd3
Jared Coleman <jaredcoleman916@gmail.com>
2018-04-16 09:07:05 -0700

    Adds options to setup script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,42 @@
 #!/bin/bash
 SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
 
+
+SSL=true
+SECRET=""
+PORT=-1
+while test $# -gt 0; do
+        case "$1" in
+                -h|--help)
+                        echo "$package - ASTRAL"
+                        echo " "
+                        echo "$package [options]"
+                        echo " "
+                        echo "options:"
+                        echo "-h, --help                show brief help"
+                        echo "--ssl                     setup API on SSL port"
+                        echo "--secret                  specify a secret phrase"
+                        echo "--port                    specify a port"
+                        exit 0
+                        ;;
+                --ssl)
+                        SSL=true
+                        shift
+                        ;;
+                --secret*)
+                        export SECRET=`echo $1 | sed -e 's/^[^=]*=//g'`
+                        shift
+                        ;;
+                --port*)
+                        export PORT=`echo $1 | sed -e 's/^[^=]*=//g'`
+                        shift
+                        ;;
+                *)
+                        break
+                        ;;
+        esac
+done
+
 # Add Mongo Keyserver
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
 echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
@@ -35,7 +71,19 @@ then
 fi
 
 # Generate config file
-astral setup-api --savedir $SCRIPT_DIR/API/config --ssl
+OPTIONS = "--savedir $SCRIPT_DIR/API/config"
+if $SECRET; then
+    $OPTIONS .= "$OPTIONS --secret $SECRET"
+fi
+if $PORT; then
+    $OPTIONS .= "$OPTIONS --port $PORT"
+fi
+if $SSL; then
+    $OPTIONS .= "$OPTIONS --ssl"
+fi
+
+echo "astral setup_api $OPTIONS"
+astral setup_api $OPTIONS
 
 # Install node packages
 cd $SCRIPT_DIR/API
