7b58ac58705072623211001e971a938dd49b046f
Viet Le <vietle8362@gmail.com>
2018-05-06 15:23:22 -0700

    Astralfile

--- phylo/app/src/main/java/edu/csulb/phylo/Astral/AstralFile.kt
+++ phylo/app/src/main/java/edu/csulb/phylo/Astral/AstralFile.kt
@@ -2,7 +2,7 @@ package edu.csulb.phylo.Astral
 
 import java.util.ArrayList
 
-class AstralRoom {
+class AstralFile {
     var owner: String? = null
     var name: String? = null
     var location: ArrayList<Double>? = null
